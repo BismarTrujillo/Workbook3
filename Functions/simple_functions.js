857ff3b88adc9fd6a57520d93bafb7f1ce6848f5
Bismar Trujillo <bismar20@hotmail.com>
2022-10-24 13:20:09 -0400

    favorite things exercise

---
+++ Functions/simple_functions.js
@@ -0,0 +1,14 @@
+function favoriteThings() {
+    console.log("Bismar" + "\n" + "Memento Mori" + "\n" + "The Strokes");
+}
+
+function whyImHere() {
+    console.log("I joined, because I want expand my knowledge in programming");
+}
+
+function favoritePlace() {
+    console.log("The beach at night, is one of my favorite places, because I love the sound of the waves");
+}
+favoriteThings();
+whyImHere();
+favoritePlace();
