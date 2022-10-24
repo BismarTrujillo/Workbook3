b3fc12794d09f6874195058bb8e2b791d918d4d9
Bismar Trujillo <bismar20@hotmail.com>
2022-10-24 13:40:49 -0400

    more_function completed

---
+++ Functions/more_functions.js
@@ -0,0 +1,8 @@
+
+function displayMaillingLabel(name, address, city, state, zip){
+console.log(name + "\n" + address + "\n" + city + ", " + state + "\t" + zip);
+}
+
+
+
+displayMaillingLabel("John Doe", "3024 S Ocean View Blvd", "Palm Beach", "FL","33480");
